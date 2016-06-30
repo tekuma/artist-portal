c3512461dc77e4fadf5ce04e1fd888f77ff0b4c6
Legatum User <legatumuser@dhcp-18-111-66-246.dyn.mit.edu>
2016-06-30 09:53:28 -0400

    implementing popvoer

--- app/views/LandingPageView.jsx
+++ app/views/LandingPageView.jsx
@@ -2,6 +2,7 @@ import React    from 'react';
 import LandingPageLayout from '../components/landing-layouts/LandingPageLayout';
 import SignUpLayout1     from '../components/landing-layouts/SignUpLayout1';
 import SignUpLayout2     from '../components/landing-layouts/SignUpLayout2';
+import {Popover} from 'react-bootstrap';
 
 
 
@@ -13,7 +14,8 @@ export default class LandingPageView extends React.Component {
         super(props);
 
         this.state = {
-            step       : 1
+            step       : 1,
+            popoverIsOpen: false
             };
     }
 
@@ -21,21 +23,40 @@ export default class LandingPageView extends React.Component {
     render() {
         switch(this.state.step) {
             case 1:
-                return <LandingPageLayout
-                        saveValues = {this.props.saveValues}
-                        nextStep   = {this.nextStep}
-                        authenticateWithGoogle = {this.props.authenticateWithGoogle}
-                        />
+                return(
+                  <div>
+                    <LandingPageLayout
+                      saveValues = {this.props.saveValues}
+                      nextStep   = {this.nextStep}
+                      authenticateWithGoogle = {this.props.authenticateWithGoogle}
+                      />
+                    <Popover
+                        placement="right"
+                        positionLeft={200}
+                        positionTop={50}
+                        title="Popover right">
+                        And here's some <strong>amazing</strong> content. Its very engaging. right?
+                    </Popover>
+                  </div>
+                );
             case 2:
-                return <SignUpLayout1
-                        saveValues = {this.props.saveValues}
-                        nextStep   = {this.nextStep}
-                        />
+                return(
+                  <div>
+                    <SignUpLayout1
+                      saveValues = {this.props.saveValues}
+                      nextStep   = {this.nextStep}
+                      />
+                  </div>
+                );
             case 3:
-                return <SignUpLayout2
-                        saveValues          = {this.props.saveValues}
-                        submitRegistration  = {this.props.submitRegistration}
-                        />
+                return(
+                  <div>
+                    <SignUpLayout2
+                      saveValues          = {this.props.saveValues}
+                      submitRegistration  = {this.props.submitRegistration}
+                      />
+                  </div>
+                );
         }
     }
 
