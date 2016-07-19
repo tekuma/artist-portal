2828f75e4a06ae515d92eddcb2f5220470d9b44b
Afika Nyati <anyati@mit.edu>
2016-07-19 17:05:56 -0400

    made cloudinary have https

--- app/App.jsx
+++ app/App.jsx
@@ -485,8 +485,9 @@ export default class App extends React.Component {
         };
         let theImage = cloudinary.image(url, args);
         let regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
-        let theURL = theImage.match(regex);
-        return theURL[0] ;
+        let theURL = theImage.match(regex)[0];
+        theURL.replace("http:", "https:"); //FIXME hacky
+        return theURL;
     }
 
     /**
